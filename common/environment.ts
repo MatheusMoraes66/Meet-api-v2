3b654b6c40aca12924b0dbd41d074a359b2a1531
MatheusMoraes66 <matheus.csharp@hotmail.com>
2022-11-19 13:13:00 -0300

    feat - [enviroment.ts]: Mudei de lugar para melhorar a organizacao.

--- common/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    server: { port: process.env.SERVER_PORT || 8081 },
-    db: { url: process.env.DB_URL || 'mongodb://127.0.0.1:27017/meat-api' },
-    security: { saltRounds: process.env.SALT_ROUNDS || 10 }
-}
