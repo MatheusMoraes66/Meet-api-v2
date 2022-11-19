45d05e69f75eed6ba00992be5e671d965d920d5d
MatheusMoraes66 <matheus.csharp@hotmail.com>
2022-11-19 12:08:02 -0300

    fix - [main.ts, server/index.ts e package.json]: Refatorando o projeto e atualizando a estrutura.

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+
+// import { usersRouter } from './routers/users';
+// import { restaurantRouter } from './routers/restaurant'
+// import { reviewsRouter } from './routers/reviews';
+
+import { createServer } from "./server";
+
+
+createServer()
